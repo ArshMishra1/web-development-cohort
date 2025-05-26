23fce171c9de34c4f7620b6b62a9d3b5120eb6a8
ArshMishra1 <161199109+ArshMishra1@users.noreply.github.com>
2025-05-26 11:52:49 +0530

    javascript

---
+++ Variable/examplevar.js
@@ -0,0 +1,17 @@
+var firstName = "arsh mishra";
+var className = "cse";
+var firstName = " mishra arsh";
+console.log(firstName);
+
+var a;
+a = "arshmishra";
+a = "kutta";
+console.log(a);
+
+let insta = {
+  Name: "Arsh",
+  followers: "888k",
+  follwing: 4,
+  discription: "iviaviniohoidajboioboidb",
+};
+
---
+++ Variable/script.js
@@ -0,0 +1,19 @@
+// <---- variable---->  we change value and redeclare name  Scope- funcation scope
+ var firstName = "ARS"
+var firstName="arsh" 
+ console.log(firstName)
+
+ // let  - we change value but do not change redeclare   Scope- Block-scoped
+ let first_Name = "abhi"
+first_Name="abhi2"
+ console.log(first_Name)
+  
+ //const we can not change both value as well as redeclare     Scope-Block-scoped
+ const First_Name= "ashish" 
+ console.log(First_Name);
+
+ 
+
+
+ 
+ 
